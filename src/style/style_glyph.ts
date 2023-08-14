e926f28b1ce5f9df689f76de4d23c4b6ad649b77
polstargps-sean <sean.chen@polstargps.com>
2023-08-14 16:20:33 +0800

    update

---
+++ src/style/style_glyph.ts
@@ -0,0 +1,21 @@
+import type {AlphaImage} from '../util/image';
+
+/**
+ * The glyph's metrices
+ */
+export type GlyphMetrics = {
+    width: number;
+    height: number;
+    left: number;
+    top: number;
+    advance: number;
+};
+
+/**
+ * A style glyph type
+ */
+export type StyleGlyph = {
+    id: number;
+    bitmap: AlphaImage;
+    metrics: GlyphMetrics;
+};
